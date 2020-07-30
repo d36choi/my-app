d712047587f7bb423e15226b8b156be50c6663a3
root <root@DESKTOP-58DIR6U.localdomain>
2020-07-30 17:09:38 +0900

    chap8.Remove&Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React, { Component } from 'react';
-import './App.css';
-import PhoneForm from './components/PhoneForm.js';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
+
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id,data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+        ? { ...info, ...data}
+        : info
+      )
+    })
   }
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
+        />
+        <PhoneInfoList 
+          data={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
         />
       </div>
     );
