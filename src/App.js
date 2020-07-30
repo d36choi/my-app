7b00c153ac5509e03af4c25c537eff22734ff37e
root <root@DESKTOP-58DIR6U.localdomain>
2020-07-30 16:23:07 +0900

    add phoneform with onclick

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import PhoneForm from './components/PhoneForm.js';
 class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
   render() {
-    return(
-    <div>
-    <PhoneForm/>
-    </div>
+    return (
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+      </div>
     );
   }
 }
